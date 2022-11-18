f0a807c56a5658500c5f46b1bc5848e7765481ce
13 <katie87827@gmail.com>
2022-11-19 02:04:39 +0800

    fix: Correct Rank Table sort-btn wording.

--- Rank/index.php
+++ Rank/index.php
@@ -62,7 +62,7 @@ include "../pdoInc.php";
     <div class="tab">
         <div class="sort">
             <p>僅列出使用次數大於 1，並依</p>
-            <button class="sort-btn active" type="build-time">建立時間</button>
+            <button class="sort-btn active" type="build-time">最新建立</button>
             <div class="bar"></div>
             <button class="sort-btn" type="times">人氣</button>
             <p>排序</p>
